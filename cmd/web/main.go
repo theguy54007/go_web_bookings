bd4d6b1268468866ca7492f74c21912fea0df83a
tommytan <tommytan@ecoworkinc.com>
2021-11-07 13:27:31 +0800

    init commit

---
+++ cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"go_udemy/bookings/pkg/config"
+	"go_udemy/bookings/pkg/handlers"
+	"go_udemy/bookings/pkg/render"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHanlders(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s\n", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
